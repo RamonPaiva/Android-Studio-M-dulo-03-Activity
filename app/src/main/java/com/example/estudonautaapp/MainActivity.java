b1c06aa3121f2401845d90b181aa0fd65163dc71
Ramon Mesquita <ramonmesquita113@gmail.com>
2019-04-13 21:16:13 -0300

    M03A03 – Formatando Atividade Principal

--- app/src/main/java/com/example/estudonautaapp/MainActivity.java
+++ app/src/main/java/com/example/estudonautaapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.estudonautaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickEquipe(View view) {
+
+    }
+
+    public void clickCursos(View view) {
+
+    }
+
+    public void clickMidia(View view) {
+        
     }
 }
